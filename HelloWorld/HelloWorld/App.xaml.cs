33d7d5e1681b7a0fcbe1c7870bb7184727aa9f5d
krao <krao@ATLM-KRAO>
2017-05-30 11:25:16 -0400

    Add project files.

---
+++ HelloWorld/HelloWorld/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using HelloWorld.Views;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace HelloWorld
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            Current.MainPage = new HelloWorldPage();
+            //SetMainPage();
+        }
+
+        public static void SetMainPage()
+        {
+            Current.MainPage = new TabbedPage
+            {
+                Children =
+                {
+                    new NavigationPage(new ItemsPage())
+                    {
+                        Title = "Browse",
+                        Icon = Device.OnPlatform("tab_feed.png",null,null)
+                    },
+                    new NavigationPage(new AboutPage())
+                    {
+                        Title = "About",
+                        Icon = Device.OnPlatform("tab_about.png",null,null)
+                    },
+                }
+            };
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld/Views/HelloWorldPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HelloWorldPage : ContentPage
+    {
+
+
+        public HelloWorldPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void SayHelloButton_OnClicked(object sender, EventArgs e)
+        {
+            var name = NameEntry.Text;
+            await DisplayAlert("Greeting", $"Hello {name}!", "Howdy");
+        }
+
+        private async void Add_OnClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Greeting", "Hello!", "Howdy");
+        }
+
+        private async void Subtract_OnClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Greeting", "Hello!", "Howdy");
+        }
+
+        private async void Multiply_OnClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Greeting", "Hello!", "Howdy");
+        }
+        private async void Divide_OnClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Greeting", "Hello!", "Howdy");
+        }
+
+        
+
+    }
+}
